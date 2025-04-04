8e3173ad788c173a5844f89731f38f3ef3c48de4
RyeinKim <ryein6896@gmail.com>
2025-04-04 12:06:14 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+// MySQL 연결
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1q2w3e4r!!A',
+    database: 'testdb',
+});
+
+app.get('/users', (req, res) => {
+    conn.query('SELECT * FROM users', (err, rows) => {
+        if (err) return res.status(500).send(err);
+        res.json(rows);
+    });
+});
+
+app.use(express.static('../frontend')); // 프론트엔드 정적 파일 제공
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
